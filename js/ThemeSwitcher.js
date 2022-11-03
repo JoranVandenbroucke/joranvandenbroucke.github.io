799f770db5ee7ee38907c92cae3f9c14bc66b3d8
Vandenbroucke Joran <joran.vandenbroucke@hotmail.com>
2022-11-03 17:19:36 +0100

    Theme Update
    all pages got a theme overhaul, I have a light and dark theme now.
    also implemented highlight.js

---
+++ js/ThemeSwitcher.js
@@ -0,0 +1,36 @@
+﻿hljs.debugMode();
+hljs.highlightAll();
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document
+        .querySelector(`link[title="dark code"]`)
+        .removeAttribute("disabled");
+    document
+        .querySelector(`link[title="light code"]`)
+        .setAttribute("disabled", "disabled");
+} else {
+    document
+        .querySelector(`link[title="light code"]`)
+        .removeAttribute("disabled");
+    document
+        .querySelector(`link[title="dark code"]`)
+        .setAttribute("disabled", "disabled");
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function (e) {
+    if (e.matches) {
+        document
+            .querySelector(`link[title="dark code"]`)
+            .removeAttribute("disabled");
+        document
+            .querySelector(`link[title="light code"]`)
+            .setAttribute("disabled", "disabled");
+    } else {
+        document
+            .querySelector(`link[title="light code"]`)
+            .removeAttribute("disabled");
+        document
+            .querySelector(`link[title="dark code"]`)
+            .setAttribute("disabled", "disabled");
+    }
+});
