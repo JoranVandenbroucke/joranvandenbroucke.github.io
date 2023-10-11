00dcb34df16c28d01616be9f7a9964b8ba370c1c
joran <joran.vandenbroucke@hotmail.com>
2023-10-11 18:10:45 +0200

    updated color scheme laid the road for things to be more robust

--- js/ThemeSwitcher.js
+++ js/ThemeSwitcher.js
@@ -1,20 +1,13 @@
 ﻿hljs.debugMode();
 hljs.highlightAll();
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document
-        .querySelector(`link[title="dark code"]`)
-        .removeAttribute("disabled");
-    document
-        .querySelector(`link[title="light code"]`)
-        .setAttribute("disabled", "disabled");
-} else {
-    document
-        .querySelector(`link[title="light code"]`)
-        .removeAttribute("disabled");
-    document
-        .querySelector(`link[title="dark code"]`)
-        .setAttribute("disabled", "disabled");
+// Function for changing theme
+function applyTheme(theme) {
+    let activeTheme = theme.matches ? 'dark code' : 'light code';
+    let inactiveTheme = theme.matches ? 'light code' : 'dark code';
+
+    document.querySelector(`link[title="${activeTheme}"]`).removeAttribute('disabled');
+    document.querySelector(`link[title="${inactiveTheme}"]`).setAttribute('disabled', 'disabled');
 }
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function (e) {
--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,23 @@
-/*!
-* Start Bootstrap - Grayscale v7.0.3 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-*/
-//
-// Scripts
-//
 
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Navbar shrink function
-    const navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
+function includeHTML() {
+    // Include the header
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            const headerContainer = document.getElementById('header');
+            const newHeader = document.createRange().createContextualFragment(data);
+            headerContainer.replaceWith(newHeader);
         });
-    }
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
+    // Include the footer
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            const footerContainer = document.getElementById('footer');
+            const newFooter = document.createRange().createContextualFragment(data);
+            footerContainer.replaceWith(newFooter);
         });
-    });
-});
+}
 
 function loadMarkDown(direction) {
     const converter = new showdown.Converter({tables: true, strikethrough: true});
@@ -84,7 +51,7 @@ function loadMarkDown(direction) {
                 headerHTML = '<dev class="my-0 py-0">';
                 headerHTML += '<h1>' + headerData.title + '</h1>';
                 headerHTML += '<p>Description: ' + headerData.description + '</p>';
-                headerHTML += '<img style="width: 5%;border-radius: 50%;margin-right:10px;" src="' + headerData.image.url + '" alt="' + headerData.image.alt + '" />';
+                headerHTML += '<img style="width: 15%;border-radius: 50%;margin-right:10px;" src="' + headerData.image.url + '" alt="' + headerData.image.alt + '" />';
                 headerHTML += '<p style="margin-right:10px;"><b>Author:</b> ' + headerData.author + '</p>';
                 headerHTML += '<p style="display:inline-block;margin-right:10px;"><b>Published on:</b> ' + headerData.pubDate + '</p>';
                 headerHTML += '<p style="display:inline-block;margin-right:10px;"><b>Read Time (Minutes):</b> ' + readTimeMinutes + '</p>';
