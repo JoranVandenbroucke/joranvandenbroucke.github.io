b7951b27851cf0e0dac5ca68f7a00da37eeb2477
joran <joran.vandenbroucke@hotmail.com>
2023-10-17 15:57:44 +0200

    Fixed Dark mode not loading code in dark mode Fixed readability of dark mode

--- js/ThemeSwitcher.js
+++ js/ThemeSwitcher.js
@@ -27,3 +27,25 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', fun
             .setAttribute("disabled", "disabled");
     }
 });
+document.addEventListener("DOMContentLoaded", function () {
+    hljs.debugMode();
+    hljs.highlightAll();
+
+    // Function for changing theme
+    function applyTheme(theme) {
+        let activeTheme = theme.matches ? "dark code" : "light code";
+        let inactiveTheme = theme.matches ? "light code" : "dark code";
+
+        document.querySelector(`link[title="${activeTheme}"]`).removeAttribute("disabled");
+        document.querySelector(`link[title="${inactiveTheme}"]`).setAttribute("disabled", "disabled");
+    }
+
+    const e = window.matchMedia("(prefers-color-scheme: dark)");
+    if (e.matches) {
+        document.querySelector(`link[title="dark code"]`).removeAttribute("disabled");
+        document.querySelector(`link[title="light code"]`).setAttribute("disabled", "disabled");
+    } else {
+        document.querySelector(`link[title="light code"]`).removeAttribute("disabled");
+        document.querySelector(`link[title="dark code"]`).setAttribute("disabled", "disabled");
+    }
+});
