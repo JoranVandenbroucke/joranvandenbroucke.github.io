37eb4f920a8fbb4ef81eeb0043a1f52921384232
joran <joran.vandenbroucke@hotmail.com>
2023-12-10 16:03:58 +0100

    All for science, science for all! Part 2

--- js/CustomHTML.js
+++ js/CustomHTML.js
@@ -45,7 +45,7 @@ class HeaderNav extends HTMLBaseElement {
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="/index.html">Joran J. C. Vandenbroucke</a>
+        <a class="navbar-brand" href="/">Joran J. C. Vandenbroucke</a>
         <button class="navbar-toggler navbar-toggler-right text-second" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -54,9 +54,9 @@ class HeaderNav extends HTMLBaseElement {
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="/index.html#about">About</a></li>
-                <li class="nav-item"><a class="nav-link" href="/index.html#portfolio">Portfolio</a></li>
-                <li class="nav-item"><a class="nav-link" href="/index.html#contact">Contact</a></li>
+                <li class="nav-item"><a class="nav-link" href="/#about">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="/#portfolio">Portfolio</a></li>
+                <li class="nav-item"><a class="nav-link" href="/#contact">Contact</a></li>
             </ul>
         </div>
     </div>
